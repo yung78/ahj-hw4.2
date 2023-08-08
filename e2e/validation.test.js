187df6618bf3568a2e018a7e13b183f6efa1de87
yung78 <alexdrum@mail.ru>
2023-08-08 18:01:25 +0300

    fixed

--- e2e/validation.test.js
+++ e2e/validation.test.js
@@ -1,16 +1,30 @@
 import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
-jest.setTimeout(15000);
+jest.setTimeout(90000);
 
 describe('Test validation form', () => {
   let browser;
   let page;
+  let server;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    server = fork('./e2e.server.js');
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        } else {
+          reject()
+        }
+      });
+    });
+
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+      // headless: false,
+      // slowMo: 100,
+      // devtools: true,
     });
 
     page = await browser.newPage();
@@ -36,7 +50,8 @@ describe('Test validation form', () => {
     await page.waitForSelector('.answer.not_valid.valid_active');
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await browser.close();
+    await server.off
   });
 });
